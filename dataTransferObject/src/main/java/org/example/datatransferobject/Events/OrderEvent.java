24460813749d8ace78bd5129681dc2a8cdf077bf
dahhou.ilyas <dahhou.ilyas@gmail.com>
2024-11-08 18:32:06 +0100

    some modification

--- dataTransferObject/src/main/java/org/example/datatransferobject/Events/OrderEvent.java
+++ dataTransferObject/src/main/java/org/example/datatransferobject/Events/OrderEvent.java
@@ -4,4 +4,34 @@ public class OrderEvent {
     private Long orderId;
     private Long prodId;
     private int prodqnt;
+
+    public OrderEvent(Long orderId, Long prodId, int prodqnt) {
+        this.orderId = orderId;
+        this.prodId = prodId;
+        this.prodqnt = prodqnt;
+    }
+
+    public Long getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(Long orderId) {
+        this.orderId = orderId;
+    }
+
+    public Long getProdId() {
+        return prodId;
+    }
+
+    public void setProdId(Long prodId) {
+        this.prodId = prodId;
+    }
+
+    public int getProdqnt() {
+        return prodqnt;
+    }
+
+    public void setProdqnt(int prodqnt) {
+        this.prodqnt = prodqnt;
+    }
 }
--- dataTransferObject/src/main/java/org/example/datatransferobject/Events/ProductEvent.java
+++ dataTransferObject/src/main/java/org/example/datatransferobject/Events/ProductEvent.java
@@ -5,4 +5,43 @@ public class ProductEvent {
     private Long prodId;
     private int prodqnt;
     private ProductStockState stockAvailability;
+
+    public ProductEvent(Long orderId, Long prodId, int prodqnt, ProductStockState stockAvailability) {
+        this.orderId = orderId;
+        this.prodId = prodId;
+        this.prodqnt = prodqnt;
+        this.stockAvailability = stockAvailability;
+    }
+
+    public Long getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(Long orderId) {
+        this.orderId = orderId;
+    }
+
+    public Long getProdId() {
+        return prodId;
+    }
+
+    public void setProdId(Long prodId) {
+        this.prodId = prodId;
+    }
+
+    public int getProdqnt() {
+        return prodqnt;
+    }
+
+    public void setProdqnt(int prodqnt) {
+        this.prodqnt = prodqnt;
+    }
+
+    public ProductStockState getStockAvailability() {
+        return stockAvailability;
+    }
+
+    public void setStockAvailability(ProductStockState stockAvailability) {
+        this.stockAvailability = stockAvailability;
+    }
 }
